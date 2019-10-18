fddb611fe22e966da76feec1690e7aaa25aa07eb
Adison Lay <adisonlay@gmail.com>
2019-10-17 17:45:07 -0700

    Revised queries to insert dog and images data

--- server/api/routes/add-dog.js
+++ server/api/routes/add-dog.js
@@ -10,12 +10,8 @@ router.post('/', function (req, res) {
   console.log('Add dog req body:', req, req.body, body);
   db.connect(() => {
     let output;
-    let query = 'INSERT INTO `dogs` (`name`, `num_dates`, `weight`, `bio`, `user_id`, `ig_url`, `birth`, `sex`, `fixed`, `breed`, `energy_lvl`) ' +
-      'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);' +
-      'SELECT @last_dog_id := LAST_INSERT_ID();';
-    body.imageURLs.forEach((url, index) => {
-      query += ("INSERT INTO `dog_images` (`dog_id`, `url`, `sort_ord`) VALUES (@last_dog_id, '" + url + "', " + index + ');');
-    });
+    const insertDogQuery = 'INSERT INTO `dogs` (`name`, `num_dates`, `weight`, `bio`, `user_id`, `ig_url`, `birth`, `sex`, `fixed`, `breed`, `energy_lvl`) ' +
+      'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
     const insertValues = [
       body.nameInput,
       0,
@@ -30,7 +26,7 @@ router.post('/', function (req, res) {
       parseInt(body.energyLevelInput)
     ];
 
-    db.query(query, insertValues, (err, result) => {
+    db.query(insertDogQuery, insertValues, (err, insertDogResult) => {
       if (err) {
         output = {
           success: false,
@@ -38,13 +34,28 @@ router.post('/', function (req, res) {
         };
         res.status(500);
       } else {
-        output = {
-          success: true,
-          data: result
-        };
-        res.status(200);
+        let insertImagesQuery = 'INSERT INTO `dog_images` (`dog_id`, `url`, `sort_ord`) VALUES ';
+        body.imageURLs.forEach((url, index, arr) => {
+          insertImagesQuery += `(${insertDogResult.insertId}, '${url}', ${index})` + ((index === arr.length - 1) ? ';' : ',');
+        });
+        db.query(insertImagesQuery, (err, insertImagesResult) => {
+          if (err) {
+            output = {
+              success: false,
+              data: err
+            };
+            res.status(500);
+          } else {
+            output = {
+              success: true,
+              insertDogData: insertDogResult,
+              insertImagesData: insertImagesResult
+            };
+            res.status(200);
+          }
+          res.json(output);
+        });
       }
-      res.json(output);
     });
   });
 });
