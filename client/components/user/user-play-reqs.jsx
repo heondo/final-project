aee6b49e1440e0c904d13069e26a85901bd6dbed
Heondo Kim <heondokim@gmail.com>
2019-10-28 16:15:09 -0700

    basic user playdates and request schedule

--- client/components/user/user-play-reqs.jsx
+++ client/components/user/user-play-reqs.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-export default function UserPlaydates(props) {
+export default function UserPlayReqs(props) {
   const [playReqs, setPlayReqs] = useState([]);
 
   useEffect(() => {
@@ -9,17 +9,108 @@ export default function UserPlaydates(props) {
 
     fetch(`/api/user-play-reqs/${props.userID}`, { signal })
       .then(res => res.json())
-      .then(res => console.log(res));
+      .then(res => {
+        if (res.error) {
+          throw new Exception(res.message);
+        }
+        let newArr = res.playdates.concat(res.requests).sort((a, b) => (a.date > b.date) ? 1 : -1);
+        setPlayReqs(newArr);
+      })
+      .catch(error => { console.error(error); });
 
     return function cleanup() {
       abortController.abort();
     };
   }, []);
 
+  const whichTypeDate = playOrReq => {
+    if ('request_id' in playOrReq) {
+      return <><Request data={playOrReq} /><hr/></>;
+    } else {
+      return <><CreatedPlaydate data={playOrReq} /> <hr /></>;
+    }
+  };
+
+  const Request = props => {
+    const { data } = props;
+    const ownerDogStyle = {
+      backgroundImage: "url('" + data.dog_image + "')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      width: '100px',
+      height: '100px'
+    };
+
+    if (data.accepted === null) {
+      return (
+        <div className="row">
+          <div className="col-lg-2" style={ownerDogStyle}></div>
+          <div className="col-lg-2">
+            <p>{`${data.other_dog_name} has not responded to meet with ${data.dog_name}`}
+              <br/><br />Request Pending</p>
+          </div>
+        </div>
+      );
+    } else if (data.accepted === 0) {
+      return (
+        <div className="row">
+          <div className="col-lg-2" style={ownerDogStyle}></div>
+          <div className="col-lg-2">
+            <p>{`${data.other_dog_name} does not wish to meet with ${data.dog_name} :(`}
+              <br /><br/><strong>Request Denied</strong></p>
+          </div>
+        </div>
+      );
+    } else if (data.accepted === 1) {
+      return (
+        <div className="row">
+          <div className="col-lg-2" style={ownerDogStyle}></div>
+          <div className="col-lg-2">
+            <p>{`${data.other_dog_name} has agreed to meet with ${data.dog_name}!`}
+              <br /><br/><strong>Request Accepted</strong></p>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  const CreatedPlaydate = props => {
+    const { data } = props;
+    const ownerDogStyle = {
+      backgroundImage: "url('" + data.dog_image + "')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      width: '100px',
+      height: '100px'
+    };
+
+    return (data.confirmed)
+      ? <div className="row">
+        <div className="col-lg-2" style={ownerDogStyle}></div>
+        <div className="col-lg-2">
+          <p>{`${data.dog_name} is meeting with ${data.req_dog_name}`}
+            <br /><br/><strong>Playdate Confirmed</strong></p>
+        </div>
+      </div>
+      : <div className="row">
+        <div className="col-lg-2" style={ownerDogStyle}></div>
+        <div className="col-lg-2">{`${data.dog_name} is not meeting with anyone...yet!`}
+          <br /><br/><strong>Playdate Still Open</strong></div>
+      </div>;
+  };
+
   return (
-    <div className="container-fluid">
+    <div className="container-fluid mx-5">
       <h4>My Playdates and Requests</h4>
-      <div className="">Something else here</div>
+      <div className="d-flex flex-column">
+        {
+          (playReqs) ? playReqs.map(pr => {
+            return whichTypeDate(pr);
+          }) : <h5>No upcoming playdates or requests</h5>
+        }
+      </div>
     </div>
   );
 }
--- server/api/routes/user-play-reqs.js
+++ server/api/routes/user-play-reqs.js
@@ -7,11 +7,33 @@ router.get('/:id', (req, res) => {
   if (!parseInt(id)) {
     res.status(400).json({ error: true, message: 'Enter a valid ID: an integer' });
   } else {
-    let query = '';
+    let requestQuery = "SELECT r.`id` AS request_id, r.`dog_id` AS 'dog_id', d_info.`name` as dog_name, d_info.`url` as dog_image, r.`accepted` AS `accepted`, FALSE AS 'playdate_created', p.`id` AS playdate_id, p.`confirmed` AS confirmed, p.`display_address`, p.`date`, od.`name` AS other_dog_name, od.`url` AS other_dog_url, od.`id` AS other_dog_id FROM `request` AS r JOIN `playdates` AS p ON r.`playdate_id` = p.`id` JOIN ( SELECT dogs.*, i.url FROM `dogs` JOIN `dog_images` AS i ON `dogs`.id = i.`dog_id` WHERE sort_ord = 0 ) AS od ON od.`id` = p.`dog_id` JOIN `dogs` AS md ON md.`id` = r.`dog_id` JOIN (SELECT d.`id`, d.`name`, i.`url` FROM `dogs` as d JOIN (SELECT * FROM `dog_images` WHERE `sort_ord`=0) as i ON d.`id` = i.`dog_id`) as d_info ON d_info.`id` = r.`dog_id` WHERE md.`user_id` = ?";
+    db.beginTransaction(err => {
+      if (err) {
+        res.status(400).json({ error: err, message: err.message }).end();
+      }
+      db.query(requestQuery, [parseInt(id)], (rqErr, reqData) => {
+        if (rqErr) {
+          res.status(400).json({ error: rqErr, message: rqErr.message }).end();
+        }
+        let playdateQuery = "SELECT p.`dog_id` AS 'dog_id', d_info.`name` as dog_name, d_info.`url` as dog_image, p.`display_address`, p.`date`, p.`id` as playdate_id, TRUE AS 'playdate_created', p.`confirmed` AS confirmed, d.`name` AS req_dog_name, d.`url` AS req_dog_url, d.`id` AS req_dog_id FROM `playdates` AS p LEFT JOIN (SELECT * FROM `request` WHERE `accepted` = 1) AS r ON p.`id` = r.`playdate_id` LEFT JOIN ( SELECT dogs.*, i.url FROM `dogs` JOIN `dog_images` AS i ON `dogs`.id = i.`dog_id` WHERE sort_ord = 0 ) AS d ON d.`id` = r.`dog_id` JOIN `dogs` AS md ON md.`id` = p.`dog_id` JOIN (SELECT d.`id`, d.`name`, i.`url` FROM `dogs` as d JOIN (SELECT * FROM `dog_images` WHERE `sort_ord`=0) as i ON d.`id` = i.`dog_id`) as d_info ON d_info.`id` = p.`dog_id` WHERE md.`user_id` = ?";
+        db.query(playdateQuery, [parseInt(id)], (pdErr, pdData) => {
+          if (pdErr) {
+            res.status(400).json({ error: pdErr, message: pdErr.message }).end();
+          }
+          db.commit(commErr => {
+            if (commErr) {
+              db.rollback(function () {
+                res.status(500).json({ error: 'Could not query both things' }).end();
+              });
+            }
+            res.status(200).json({ success: true, playdates: pdData, requests: reqData }).end();
+          });
+        });
+      });
+    });
     // GET FOR THIS USER, get their dogs. Get playdates and requestse associated with the dogs
-
   }
-  res.json({ id });
 });
 
 module.exports = router;
